9064d817dd3f13e032b43b49512c2f232e87b9a9
Ben Clark <benjamin.r.clark@gmail.com>
2015-12-17 21:31:08 +0000

    remove spring-cloud-aws-autoconfigure as it seemed to be doing more harm than good

--- src/main/java/org/cateproject/AWSConfiguration.java
+++ src/main/java/org/cateproject/AWSConfiguration.java
@@ -1,12 +1,16 @@
 package org.cateproject;
 
+import org.springframework.cloud.aws.context.config.annotation.EnableContextCredentials;
 import org.springframework.cloud.aws.context.config.annotation.EnableContextInstanceData;
+import org.springframework.cloud.aws.context.config.annotation.EnableContextRegion;
 import org.springframework.cloud.aws.context.config.annotation.EnableStackConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Profile("aws")
 @Configuration
+@EnableContextCredentials(instanceProfile=true)
+@EnableContextRegion(autoDetect=true)
 @EnableContextInstanceData
 @EnableStackConfiguration
 public class AWSConfiguration {

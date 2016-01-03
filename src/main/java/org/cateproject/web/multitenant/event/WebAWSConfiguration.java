0762e6de1e0e88293f2cb3c079ed052c41b87d9e
Ben Clark <benjamin.r.clark@gmail.com>
2016-01-03 20:18:01 +0000

    Added sns argument resolvers

--- src/main/java/org/cateproject/web/multitenant/event/WebAWSConfiguration.java
+++ src/main/java/org/cateproject/web/multitenant/event/WebAWSConfiguration.java
@@ -1,13 +1,25 @@
 package org.cateproject.web.multitenant.event;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.aws.messaging.endpoint.NotificationMessageHandlerMethodArgumentResolver;
+import org.springframework.cloud.aws.messaging.endpoint.NotificationStatusHandlerMethodArgumentResolver;
+import org.springframework.cloud.aws.messaging.endpoint.NotificationSubjectHandlerMethodArgumentResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.amazonaws.services.sns.AmazonSNS;
 
 @Profile("aws")
 @Configuration
 public class WebAWSConfiguration extends WebMvcConfigurerAdapter{
+
+    @Autowired
+    private AmazonSNS amazonSns;
 
     @Bean
     public SnsEndpointController snsEndpointController() {
@@ -17,5 +29,27 @@ public class WebAWSConfiguration extends WebMvcConfigurerAdapter{
     @Bean
     public SnsSubscriptionRegistrar snsSubscriptionRegistrar() {
         return new SnsSubscriptionRegistrar();
+    }
+
+    @Bean
+    public NotificationStatusHandlerMethodArgumentResolver notificationStatusHandlerMethodArgumentResolver() {
+        return new NotificationStatusHandlerMethodArgumentResolver(amazonSns);
+    }
+
+    @Bean
+    public NotificationMessageHandlerMethodArgumentResolver notificationMessageHandlerMethodArgumentResolver() {
+        return new NotificationMessageHandlerMethodArgumentResolver();
+    }
+
+    @Bean
+    public NotificationSubjectHandlerMethodArgumentResolver notificationSubjectHandlerMethodArgumentResolver() {
+        return new NotificationSubjectHandlerMethodArgumentResolver();
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(notificationStatusHandlerMethodArgumentResolver());
+        argumentResolvers.add(notificationMessageHandlerMethodArgumentResolver());
+        argumentResolvers.add(notificationSubjectHandlerMethodArgumentResolver());
     }
 }
